78e68971277c6c91711dee4beda21a5e9f57e46e
Susheel BG <lifeafterruin2@gmail.com>
2026-06-19 22:38:55 +0530

    zoom disabled

--- frontend/android/app/src/main/java/com/milkmaatu/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/milkmaatu/app/MainActivity.java
@@ -1,5 +1,21 @@
 package com.milkmaatu.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Access the WebView settings and strictly disable zooming
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            WebSettings settings = this.bridge.getWebView().getSettings();
+            settings.setSupportZoom(false);
+            settings.setBuiltInZoomControls(false);
+            settings.setDisplayZoomControls(false);
+            settings.setTextZoom(100); // Forces text size to stay constant
+        }
+    }
+}
